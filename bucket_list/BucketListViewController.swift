b213ca15be5f323376fff0a744fc3f4f9d661724
Tri Pham <phamqtri1992@gmail.com>
2017-02-13 15:00:08 -0800

    First Commit

---
+++ bucket_list/BucketListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  bucket_list
+//
+//  Created by Tri Pham on 2/13/17.
+//  Copyright © 2017 Tri Pham. All rights reserved.
+//
+
+import UIKit
+
+class BucketListViewController: UITableViewController {
+    var tasks = ["Wake up", "Change clothes", "Go to the dojo"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Loaded")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyTask", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+}
+
+		
